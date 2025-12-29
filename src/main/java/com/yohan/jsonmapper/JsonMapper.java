2ab3db2d8fa09b4e1bbff956ba2d54d7af2df18c
Yohan Senanayake <yohansenanayake4321@gmail.com>
2025-12-29 14:29:47 +0530

    Basic JSON Mapper Implemented

--- src/main/java/com/yohan/jsonmapper/JsonMapper.java
+++ src/main/java/com/yohan/jsonmapper/JsonMapper.java
@@ -1,8 +1,44 @@
 package com.yohan.jsonmapper;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class JsonMapper {
 
     public String toJson(Object obj) {
-        return null;
+        var clazz = obj.getClass();
+        var fields = clazz.getDeclaredFields();
+
+        return Arrays.stream(fields)
+                .map(field -> {
+                        var name = field.getName();
+                        var value = asJsonValue(getFieldValue(field, obj));
+                        return "\"%s\":%s" .formatted(name,value);
+
+                }).collect(Collectors.joining(",","{","}"));
+    }
+
+    private Object getFieldValue(Field field, Object obj){
+        try {
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }finally {
+            field.setAccessible(false);
+        }
+    }
+
+    private String asJsonValue(Object value) {
+        if(isPrimitive(value)){
+            return String.valueOf(value);
+        }
+        //characters and string have quotes
+        return '"'+ String.valueOf(value)+'"';
+    }
+
+    private boolean isPrimitive(Object value) {
+        return value instanceof Boolean || value instanceof Number ;
     }
 }
--- src/test/java/com/yohan/jsonmapper/JsonMapperTest.java
+++ src/test/java/com/yohan/jsonmapper/JsonMapperTest.java
@@ -1,13 +1,77 @@
 package com.yohan.jsonmapper;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JsonMapperTest {
 
+    private JsonMapper jsonMapper = new JsonMapper();
+
     @Test
-    void placeHolder() {
-        assertTrue(true);
+    @DisplayName("Should return {} for empty objects")
+    void shouldReturnEmptyObject(){
+        //given
+        var input = new Object();
+        //when
+        var actual = jsonMapper.toJson(input);
+        //then
+        var expected = "{}";
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("Should correctly map string fields in objects")
+    void shouldCorrectlyMapStringFields() {
+        //given
+        record Person(String firstName, String lastName){}
+        var input = new Person("John", "Doe");
+        //when
+        var actual = jsonMapper.toJson(input);
+        //then
+        //{"firstName":"John","lastName":"Doe"}
+        var expected = "{\"firstName\":\"John\",\"lastName\":\"Doe\"}";
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("Should correctly map Integer fields")
+    void shouldCorrectlyMapIntegerFields() {
+        //given
+        record BlogPost(String title, int likes){}
+        var input = new BlogPost("JSON Mapper Test", 42);
+        //when
+        var actual = jsonMapper.toJson(input);
+        //then
+        var expected = "{\"title\":\"JSON Mapper Test\",\"likes\":42}";
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("Should correctly map primitive fileds")
+    void shouldCorrectlyMapPrimitiveFields() {
+        //given
+        record Stock(String title , float price){}
+        var input = new Stock("Bitcoin", 42.345f);
+        //when
+        var actual = jsonMapper.toJson(input);
+        //then
+        var expected = "{\"title\":\"Bitcoin\",\"price\":42.345}";
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("Should correctly map boolean fileds")
+    void shouldCorrectlyMapBooleanFields() {
+        //given
+        record Stock(String title , boolean available){}
+        var input = new Stock("Bitcoin", false);
+        //when
+        var actual = jsonMapper.toJson(input);
+        //then
+        var expected = "{\"title\":\"Bitcoin\",\"available\":false}";
+        Assertions.assertEquals(expected,actual);
     }
 }
